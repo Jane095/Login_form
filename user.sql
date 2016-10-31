543b673e3d291b3419fd7e143d9aab02440143f2
Jane095 <jane095@yandex.ru>
2016-10-31 19:34:38 +0300

    commit

---
+++ user.sql
@@ -0,0 +1,27 @@
+﻿--
+-- Скрипт сгенерирован Devart dbForge Studio for MySQL, Версия 7.1.13.0
+-- Домашняя страница продукта: http://www.devart.com/ru/dbforge/mysql/studio
+-- Дата скрипта: 31.10.2016 19:09:43
+-- Версия сервера: 5.5.5-10.1.10-MariaDB
+-- Версия клиента: 4.1
+--
+
+
+SET NAMES 'utf8';
+
+INSERT INTO login_form.user(id, login, name, email, password, education, address, phone) VALUES
+(22, 'jane', 'gdfg', 'janeeee@yandex.ru', '$2y$10$XB.LBTdUDfzBrwaIY/kDJOmuzO7PWD5Iq8wVgwLVPqPVXt7OAk3D6', 'dh', 'ddhdfh', x'5757457547');
+INSERT INTO login_form.user(id, login, name, email, password, education, address, phone) VALUES
+(23, 'marina', '111111', 'marina@yandex.ru', '$2y$10$X9FDFfHGVp5q4151CyWenOfrrSZIvSoPozx0oNfFx2YQBIHkXIZ8y', '1111111', '1111111', x'111111111');
+INSERT INTO login_form.user(id, login, name, email, password, education, address, phone) VALUES
+(24, 'roma', '5', 'roma@yandex.ru', '$2y$10$sPBgW/BgiDOxXll7k0ccQugOoaQZ8JpTWEb50ufjoVN8PsQ4r7f7a', '5', '5', x'5');
+INSERT INTO login_form.user(id, login, name, email, password, education, address, phone) VALUES
+(25, 'dasha', NULL, 'dasha@yandex.ru', '$2y$10$daI7HYRp3xqWRRLG8wuNOupDCnl4XxdAJa1ExGgyUUnGaQbukTkgS', NULL, NULL, NULL);
+INSERT INTO login_form.user(id, login, name, email, password, education, address, phone) VALUES
+(26, 'farsh', NULL, 'farsh@yandex.ru', '$2y$10$AktB24mfSrAJX69tt6p/AeAIEuoiBmdltJ02WVuDUyKfJsNZGXPwW', NULL, NULL, NULL);
+INSERT INTO login_form.user(id, login, name, email, password, education, address, phone) VALUES
+(27, 'nastya', NULL, 'nastya@yandex.ru', '$2y$10$SYhPjZt9oYU04eKM/vUnTendkkx8fbrTzGpNrrqrxx03LUfwGuBca', NULL, NULL, NULL);
+INSERT INTO login_form.user(id, login, name, email, password, education, address, phone) VALUES
+(28, 'masha', NULL, 'masha@yandex.ru', '$2y$10$KTc.KMGkeh5eyBAJSj5qeutweMk01.cYLi1I9nGYpr5rcdtVI6lwq', NULL, NULL, NULL);
+INSERT INTO login_form.user(id, login, name, email, password, education, address, phone) VALUES
+(29, 'valya', NULL, 'valya@yandex.ru', '$2y$10$xHrukOgSpqZydHBii13DpOQ9bQBXviJTvdHCJmiYpFHCg6b9Y1kCW', NULL, NULL, NULL);
